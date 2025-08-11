ebb1d1cf7dac45f8a6b8903ec811327196a11e75
Andreas Andrade <aadredre@umich.edu>
2025-08-11 01:25:46 -0400

    rating cleanup, setup for full passage credit

---
+++ Migrations/20250811052342_rating.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DuneLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class rating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullPassageCredit",
+                table: "Passage");
+
+            migrationBuilder.AddColumn<float>(
+                name: "AverageRating",
+                table: "Passage",
+                type: "REAL",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageRating",
+                table: "Passage");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullPassageCredit",
+                table: "Passage",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
