c46dd349f82b10aa723929bd066603c9c559b9ca
Andreas Andrade <aadredre@umich.edu>
2025-09-03 00:35:59 -0400

    3/4 of God Emperor of Dune passages in

--- obj/Debug/net9.0/DuneLibrary.AssemblyInfo.cs
+++ obj/Debug/net9.0/DuneLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DuneLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56740d0ce9a2d4e0298e2e3bcde6927c102cb483")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d0dfe807e1ea78234396901da2dcf88fa5d557")]
 [assembly: System.Reflection.AssemblyProductAttribute("DuneLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DuneLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
