eb0241f1867b3bb0ea715de0d46337fd259da104
Andreas Andrade <aadredre@umich.edu>
2025-09-15 00:08:36 -0400

    Heretics of Dune Passages Complete

--- obj/Debug/net9.0/DuneLibrary.AssemblyInfo.cs
+++ obj/Debug/net9.0/DuneLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DuneLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c187b0fcb632f6b153d828a4be4c666d3964b6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ffd611c1e3d4fdb8cc7ea3bc8d982611aeb32bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DuneLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DuneLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
